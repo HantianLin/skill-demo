1b1aee344f688962b27083024be6c77b64df1a08
HantianLin <1018753839wl@gmail.com>
2022-05-01 19:09:20 -0700

    create a test file

--- skillDemo.java
+++ skillDemo.java
@@ -1,2 +1,5 @@
 public class skillDemo{
+    public static int sum (int int1, int int2) {
+        return int1+int2;
+    }
 }
---
+++ skillDemoTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class skillDemoTest {
+    @Test
+    public void testSum() {
+        assertEquals(3, skillDemo.sum(1, 1));
+    }
+}
